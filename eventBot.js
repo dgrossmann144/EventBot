08e3034cc24f7c12dd0d663f3fd15f5aa120bc13
dgrossmann144 <dgrossmann144@gmail.com>
2018-09-02 10:38:08 -0400

    Message is saved when an event is created, but if it is used too soon then it crashes

--- eventBot.js
+++ eventBot.js
@@ -6,8 +6,40 @@ var client = new discord.Client();
 client.on('message', msg => {
     if (msg.content.charAt(0) === ':' && msg.content.charAt(1) === ':') {
         var command = msg.content.slice(2).split(' ')[0];
-        msg.channel.send(msg.author + ' used the ' + command + ' command');
+
+        switch(command) {
+            case 'createEvent':
+                var args = msg.content.split(' ');
+                if (args.length != 5) {
+                    msg.channel.send('That event does not have the correct number of  arguments, use help to view the arguments');
+                    break;
+                }
+                var newEvent = new UserEvent(args[1], args[2], args[3], args[4], msg.member.nickname, msg.channel);
+
+                break;
+            case 'help':
+                //TODO update help command with list of all commands
+
+                break;
+            default:
+                msg.channel.send('That is not a valid command, use help to list all commands.');
+        }
     }
 });
 
 client.login(auth.token);
+
+function UserEvent(eventName, eventDescription, eventDate, eventTime, createdBy, channel) {
+    this.eventName = eventName;
+    this.eventDescription = eventDescription;
+    this.eventDate = eventDate;
+    this.eventTime = eventTime;
+    this.createdBy = createdBy;
+    this.channel = channel;
+    var attendees = [];
+    var potentialAttendees = [];
+
+    var eventMessage;
+    channel.send('test message').then(message => eventMessage = message);
+    //TODO write message and put it in a message var
+}
