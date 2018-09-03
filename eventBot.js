be0aa7b997658139467a5f611408625b7ea056a6
dgrossmann144 <dgrossmann144@gmail.com>
2018-09-03 15:19:59 -0400

    Got reactions working to add to lists (needs testing)

--- eventBot.js
+++ eventBot.js
@@ -4,14 +4,16 @@ var discord = require("discord.js");
 var client = new discord.Client();
 var events = [];
 
+client.login(auth.token);
+
 client.on('message', msg => {
-    if (!msg.content.startsWith('::')) {
+    if (!msg.content.startsWith(';;')) {
         return;
     }
-    var command = msg.content.slice(2).split(' ')[0];
+    var command = msg.content.slice(2).split(';')[0];
     switch(command) {
         case 'createEvent':
-            var args = msg.content.split(' ');
+            var args = msg.content.slice(2).split(';');
             if (args.length != 5) {
                 msg.channel.send('That event does not have the correct number of  arguments, use help to view the arguments');
                 break;
@@ -31,26 +33,56 @@ client.on('message', msg => {
 client.on('messageReactionAdd', (messageReaction, user) => {
     if(user.id !== client.user.id) {
         for(var i = 0; i < events.length; i++) {
-            console.log("event tostring: " + events[i].toString());
-            console.log(events[i].isMessageSent);
             if(events[i].isMessageSent && messageReaction.message.id === events[i].eventMessage.id) {
                 if(messageReaction.emoji.id === '409344837997821952') {
-                    events[i].attendees.push(user);
-                    for(var test = 0; test < events[i].attendees.length; i++) {
-                        console.log(events[i].attendees[test]);
+                    events[i].addAttendee(user);
+                    //debug
+                    for(var test = 0; test < events[i].attendees.length; test++) {
+                        console.log(events[i].attendees[test].username);
                     }
+
+                    break;
                 } else if (messageReaction.me) {
-                    events[i].potentialAttendees.push(user);
-                    for(var test = 0; test2 < events[i].potentialAttendees.length; i++) {
-                        console.log(events[i].potentialAttendees[test2]);
+                    events[i].addPotentialAttendee(user);
+                    //debug
+                    for(var test2 = 0; test2 < events[i].potentialAttendees.length; test2++) {
+                        console.log(events[i].potentialAttendees[test2].username);
                     }
+
+                    break;
                 }
             }
         }
     }
 });
 
-client.login(auth.token);
+client.on('messageReactionRemove', (messageReaction, user) => {
+    if(user.id !== client.user.id) {
+        for(var i = 0; i < events.length; i++) {
+            if(events[i].isMessageSent && messageReaction.message.id === events[i].eventMessage.id) {
+                if(messageReaction.emoji.id === '409344837997821952') {
+                    var removeIndex = events[i].attendees.indexOf(user);
+                    events[i].removeAttendee(removeIndex);
+                    //debug
+                    for(var test = 0; test < events[i].attendees.length; test++) {
+                        console.log(events[i].attendees[test].username);
+                    }
+
+                    break;
+                } else if (messageReaction.me) {
+                    var removeIndex = events[i].potentialAttendees.indexOf(user);
+                    events[i].removePotentialAttendee(removeIndex);
+                    //debug
+                    for(var test2 = 0; test2 < events[i].potentialAttendees.length; test2++) {
+                        console.log(events[i].potentialAttendees[test2].username);
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+});
 
 function UserEvent (eventName, eventDescription, eventDate, eventTime, createdBy, channel) {
     this.eventName = eventName;
@@ -67,31 +99,48 @@ function UserEvent (eventName, eventDescription, eventDate, eventTime, createdBy
 
     channel.send(createdBy.user + ' has created an event: ' + eventName +
                  '\n' + eventDescription +
-                 '\n' + 'It will take place on ' + eventDate + ' at ' + eventTime +
+                 '\n' + 'It will take place ' + eventDate + ' at ' + eventTime +
                  '\n' + 'React with ' + channel.guild.emojis.get('409344837997821952') + ' if you are attending, or ❓ if you might be attending.').then(message => {
         self.eventMessage = message
         self.isMessageSent = true;
 
         //id for party_wurmple: 409344837997821952
-        self.eventMessage.react(eventMessage.guild.emojis.get('409344837997821952')).then(() => {
+        self.eventMessage.react(self.eventMessage.guild.emojis.get('409344837997821952')).then(() => {
             self.eventMessage.react('❓');
         });
 
         events.push(this);
-
-        // lists name and id of all custom emojis on a server
+        // debug lists name and id of all custom emojis on a server
         // function listEmojis(curEmoji) {
         //     console.log(curEmoji.name + ' ' + curEmoji.id);
         //     return true;
         // }
         // eventMessage.guild.emojis.every(listEmojis)
-
     }, () => {
         channel.send('There was an error setting up the event, try again.');
     });
-    //TODO write message and put it in a message var
 
-    function toString() {
-        return this.eventName.toString();
+    UserEvent.prototype.toString = function toString() {
+        return this.eventName + ' by ' + this.createdBy.nickname.toString();
+    }
+
+    UserEvent.prototype.addAttendee = function(user) {
+        this.attendees.push(user);
+        //TODO edit message to update attendee list
+    }
+
+    UserEvent.prototype.removeAttendee = function(index) {
+        this.attendees.splice(index, 1);
+        //TODO edit message to update attendee list
+    }
+
+    UserEvent.prototype.addPotentialAttendee = function(user) {
+        this.potentialAttendees.push(user);
+        //TODO edit message to update maybe list
+    }
+
+    UserEvent.prototype.removePotentialAttendee = function(index) {
+        this.potentialAttendees.splice(index, 1);
+        //TODO edit message to update maybe list
     }
 }
