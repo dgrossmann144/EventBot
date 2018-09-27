ec27657c149460d05adc9a08f9ca76be473d1b87
dgrossmann144 <dgrossmann144@gmail.com>
2018-09-27 14:17:43 -0400

    Changed some syntax and added announce

--- eventBot.js
+++ eventBot.js
@@ -6,7 +6,9 @@ var events = [];
 
 process.on('SIGINT', () => {
     for (let event of events) {
-        event.eventMessage.unpin();
+        if(event.isAnnounced) {
+            event.eventMessage.unpin();
+        }
     }
 
     client.destroy();
@@ -27,14 +29,16 @@ client.on('message', msg => {
                 break;
             }
             if(lookupEvent(args[1]) === null) {
-                new UserEvent(args[1], args[2], args[3], args[4], msg.member, msg.channel);
+                new UserEvent(args[1], args[2], args[3], args[4], msg.member);
+                msg.channel.send('Successfullly created event.');
+                break;
             } else {
                 msg.channel.send('There is already an event with that name.');
                 break;
             }
 
             break;
-        case 'changeName':
+        case 'setName':
             var args = msg.content.slice(2).split(';');
             if(args.length != 3) {
                 msg.channel.send('That command does not have the correct number of arguments, use help to view the arguments.');
@@ -59,11 +63,13 @@ client.on('message', msg => {
                 }
             }
             event.eventName = args[2];
-            event.updateAttendees();
+            if(event.isAnnounced) {
+                event.updateAttendees();
+            }
             msg.channel.send('Successfully updated event name.');
 
             break;
-        case 'changeDescription':
+        case 'setDescription':
             var args = msg.content.slice(2).split(';');
             if(args.length != 3) {
                 msg.channel.send('That command does not have the correct number of arguments, use help to view the arguments.');
@@ -82,11 +88,13 @@ client.on('message', msg => {
             }
 
             event.eventDescription = args[2];
-            event.updateAttendees();
+            if(event.isAnnounced) {
+                event.updateAttendees();
+            }
             msg.channel.send('Successfully updated event description.');
 
             break;
-        case 'changeDate':
+        case 'setDate':
             var args = msg.content.slice(2).split(';');
             if(args.length != 3) {
                 msg.channel.send('That command does not have the correct number of arguments, use help to view the arguments.');
@@ -105,11 +113,13 @@ client.on('message', msg => {
             }
 
             event.eventDate = args[2];
-            event.updateAttendees();
+            if(event.isAnnounced) {
+                event.updateAttendees();
+            }
             msg.channel.send('Successfully updated event date.');
 
             break;
-        case 'changeTime':
+        case 'setTime':
             var args = msg.content.slice(2).split(';');
             if(args.length != 3) {
                 msg.channel.send('That command does not have the correct number of arguments, use help to view the arguments.');
@@ -128,9 +138,36 @@ client.on('message', msg => {
             }
 
             event.eventTime = args[2];
-            event.updateAttendees();
+            if(event.isAnnounced) {
+                event.updateAttendees();
+            }
             msg.channel.send('Successfully updated event time.');
 
+            break;
+        case 'announce':
+            var args = msg.content.slice(2).split(';');
+            if(args.length != 2) {
+                msg.channel.send('That command does not have the correct number of arguments, use help to view the arguments.');
+                break;
+            }
+
+            var event = lookupEvent(args[1]);
+            if(event === null) {
+                msg.channel.send('There is no event that goes by that name.');
+                break;
+            }
+
+            if(event.isAnnounced) {
+                msg.channel.send('That event is already announced.');
+                break;
+            }
+
+            if(event.createdBy !== msg.member) {
+                msg.channel.send('Only the creator of an event can announce it.');
+                break;
+            }
+
+            event.announceEvent(msg.channel);
             break;
         case 'listEvents':
             var args = msg.content.slice(2).split(';');
@@ -139,18 +176,28 @@ client.on('message', msg => {
                 break;
             }
 
-            var listOfEvents = 'List of event names: ';
+            var announcedEvents = 'Announced event names: ';
+            var unannouncedEvents = 'Unannounced event names: ';
             for(let event of events) {
-                listOfEvents += event.eventName + ', ';
+                if (event.isAnnounced) {
+                    announcedEvents += event.eventName + ', ';
+                } else {
+                    unannouncedEvents += event.eventName + ', ';
+                }
             }
-            if(events.length > 0) {
-                listOfEvents = listOfEvents.substr(0, listOfEvents.length - 2);
+            if(announcedEvents !== 'Announced event names: ') {
+                announcedEvents = announcedEvents.substr(0, announcedEvents.length - 2);
             } else {
-                listOfEvents = 'There are no events.';
+                announcedEvents = 'There are no announced events.';
             }
 
-            msg.channel.send(listOfEvents);
+            if(unannouncedEvents !== 'Unannounced event names: ') {
+                unannouncedEvents = unannouncedEvents.substr(0, unannouncedEvents.length - 2);
+            } else {
+                unannouncedEvents = 'There are no unannounced events.';
+            }
 
+            msg.channel.send(announcedEvents + '\n' + unannouncedEvents);
             break;
         case 'delete':
             var args = msg.content.slice(2).split(';');
@@ -170,21 +217,25 @@ client.on('message', msg => {
                 break;
             }
 
-            event.eventMessage.unpin();
+            if(event.isAnnounced) {
+                event.eventMessage.unpin();
+            }
             events.splice(events.indexOf(event), 1);
             msg.channel.send('Event deleted.');
 
             break;
         case 'help':
-            msg.channel.send('createEvent;eventName;eventDescription;eventDate;eventTime\n' +
-                             'changeName;eventName;newName\n' +
-                             'changeDescription;eventName;newDescription\n' +
-                             'changeDate;eventName;newDate\n' +
-                             'changeTime;eventName;newTime\n' +
+            msg.channel.send('createEvent;eventName\n' +
+                             'setName;eventName;newName\n' +
+                             'setDescription;eventName;newDescription\n' +
+                             'setDate;eventName;newDate\n' +
+                             'setTime;eventName;newTime\n' +
+                             'announceEvent;eventName\n' +
+                             'listEvents\n' +
                              'delete;eventName');
             break;
         default:
-            msg.channel.send('That is not a valid command, use help to list all commands.');
+            msg.channel.send('That is not a valid command, type ";;help" to list all commands.');
     }
 });
 
@@ -226,80 +277,95 @@ client.on('messageReactionRemove', (messageReaction, user) => {
     }
 });
 
-function UserEvent (eventName, eventDescription, eventDate, eventTime, createdBy, channel) {
+function UserEvent (eventName, eventDescription, eventDate, eventTime, createdBy) {
     this.eventName = eventName;
     this.eventDescription = eventDescription;
     this.eventDate = eventDate;
     this.eventTime = eventTime;
     this.createdBy = createdBy;
-    this.channel = channel;
+    this.channel;
     this.isMessageUpdated = false;
     this.attendees = new Set();
     this.potentialAttendees = new Set();
     this.notAttending = new Set();
     this.eventMessage;
+    this.isAnnounced = false;
+    events.push(this);
 
-    channel.send(createdBy.user + ' has created an event: ' + this.eventName +
-                 '\n' + this.eventDescription +
-                 '\n' + 'It will take place ' + this.eventDate + ' at ' + this.eventTime +
-                 '\n' + 'React with ' + channel.guild.emojis.get('409344837997821952') + ' if you are attending, ❓ if you might be attending, or ' + channel.guild.emojis.get('292824502969303041') + ' if you are not attending.').then(message => {
-        this.eventMessage = message
-        this.eventMessage.pin();
-        this.isMessageUpdated = true;
+    this.announceEvent = function(channel) {
+        if (typeof this.eventDescription === 'undefined' ||
+            typeof this.eventDate === 'undefined' ||
+            typeof this.eventTime === 'undefined') {
+            channel.send('The description, date, or time was left undefined.');
+        } else {
+            channel.send(this.createdBy.user + ' has created an event: ' + this.eventName +
+                     '\n' + this.eventDescription +
+                     '\nIt will take place ' + this.eventDate + ' at ' + this.eventTime +
+                     '\nReact with ' + channel.guild.emojis.get('409344837997821952') + ' if you are attending, ❓ if you might be attending, or ' + channel.guild.emojis.get('292824502969303041') + ' if you are not attending.' +
+                     '\nAttending (0): ' +
+                     '\nMaybe Attending (0): ' +
+                     '\nNot Attending (0): ').then(message => {
+                this.eventMessage = message;
+                this.channel = channel;
+                //TODO uncomment this
+                // this.eventMessage.pin();
+                this.isMessageUpdated = true;
+                this.isAnnounced = true;
 
-        this.eventMessage.react(this.eventMessage.guild.emojis.get('409344837997821952')).then(() => {
-            this.eventMessage.react('❓').then(() => {
-                this.eventMessage.react(this.eventMessage.guild.emojis.get('292824502969303041'));
+                this.eventMessage.react(this.eventMessage.guild.emojis.get('409344837997821952')).then(() => {
+                    this.eventMessage.react('❓').then(() => {
+                        this.eventMessage.react(this.eventMessage.guild.emojis.get('292824502969303041'));
+                    });
+                });
+
+            // debug lists name and id of all custom emojis on a server
+            // function listEmojis(curEmoji) {
+            //     console.log(curEmoji.name + ' ' + curEmoji.id);
+            //     return true;
+            // }
+            // this.eventMessage.guild.emojis.every(listEmojis);
+            }, () => {
+                channel.send('There was an error setting up the event, try again.');
             });
-        });
+        }
+    }
 
-        events.push(this);
-        // debug lists name and id of all custom emojis on a server
-        // function listEmojis(curEmoji) {
-        //     console.log(curEmoji.name + ' ' + curEmoji.id);
-        //     return true;
-        // }
-        // this.eventMessage.guild.emojis.every(listEmojis);
-    }, () => {
-        channel.send('There was an error setting up the event, try again.');
-    });
-
-    UserEvent.prototype.addAttendee = function(user) {
+    this.addAttendee = function(user) {
         this.attendees.add(user);
         this.updateAttendees();
     }
 
-    UserEvent.prototype.removeAttendee = function(user) {
+    this.removeAttendee = function(user) {
         this.attendees.delete(user);
         this.updateAttendees();
     }
 
-    UserEvent.prototype.addPotentialAttendee = function(user) {
+    this.addPotentialAttendee = function(user) {
         this.potentialAttendees.add(user);
         this.updateAttendees();
     }
 
-    UserEvent.prototype.removePotentialAttendee = function(user) {
+    this.removePotentialAttendee = function(user) {
         this.potentialAttendees.delete(user);
         this.updateAttendees();
     }
 
-    UserEvent.prototype.addNotAttending = function(user) {
+    this.addNotAttending = function(user) {
         this.notAttending.add(user);
         this.updateAttendees();
     }
 
-    UserEvent.prototype.removeNotAttending = function(user) {
+    this.removeNotAttending = function(user) {
         this.notAttending.delete(user);
         this.updateAttendees();
     }
 
-    UserEvent.prototype.updateAttendees = function() {
+    this.updateAttendees = function() {
         this.isMessageUpdated = false;
         var updatedText = this.createdBy.user + ' has created an event: ' + this.eventName +
                      '\n' + this.eventDescription +
                      '\n' + 'It will take place ' + this.eventDate + ' at ' + this.eventTime +
-                     '\n' + 'React with ' + channel.guild.emojis.get('409344837997821952') + ' if you are attending, ❓ if you might be attending, or ' + channel.guild.emojis.get('292824502969303041') + ' if you are not attending.' +
+                     '\n' + 'React with ' + this.channel.guild.emojis.get('409344837997821952') + ' if you are attending, ❓ if you might be attending, or ' + this.channel.guild.emojis.get('292824502969303041') + ' if you are not attending.' +
                      '\nAttending (' + this.attendees.size +'): ';
         for(let attendee of this.attendees) {
             updatedText += attendee + ', ';
@@ -326,7 +392,7 @@ function UserEvent (eventName, eventDescription, eventDate, eventTime, createdBy
         });
     }
 
-    UserEvent.prototype.toString = function toString() {
+    this.toString = function() {
         return this.eventName + ' by ' + this.createdBy.nickname.toString();
     }
 }
